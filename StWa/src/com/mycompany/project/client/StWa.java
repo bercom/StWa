1cf6610ceae732fe814638b3cee5888c45958969
poul <bercom@gmail.com>
2014-12-04 00:17:32 +0100

    Clickme button fra sample-code fjernet

--- StWa/src/com/mycompany/project/client/StWa.java
+++ StWa/src/com/mycompany/project/client/StWa.java
@@ -25,17 +25,7 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class StWa implements EntryPoint {
-	private Button clickMeButton;
 	public void onModuleLoad() {
 		RootPanel rootPanel = RootPanel.get();
-
-		clickMeButton = new Button();
-		rootPanel.add(clickMeButton);
-		clickMeButton.setText("Click me!");
-		clickMeButton.addClickHandler(new ClickHandler(){
-			public void onClick(ClickEvent event) {
-				Window.alert("Hello, GWT World!");
-			}
-		});
 	}
 }
